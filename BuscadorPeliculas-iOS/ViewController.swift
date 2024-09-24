f7bec8d8a2387ada4bf4957b865876440e8b14bd
zitrokv <victor.vialasc@gmail.com>
2024-09-24 09:36:59 +0200

    Initial Commit

---
+++ BuscadorPeliculas-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuscadorPeliculas-iOS
+//
+//  Created by Mañanas on 24/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
